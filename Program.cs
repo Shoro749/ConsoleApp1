521e5991ff736c02e614a1d274ad0415ff673b2d
Shoro749 <romanbro749@gmail.com>
2023-06-24 17:26:16 +0300

    Task 3 completed

--- Program.cs
+++ Program.cs
@@ -6,37 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /*Користувач вводить з клавіатури чотири цифри. Необхідно створити число, яке містить ці цифри. 
+            Наприклад, якщо з клавіатури введено 1, 5, 7, 8 тоді потрібно
+            сформувати число 1578.*/
             try
             {
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
-
-                if (num > 0 && num < 100)
-                {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
+                Console.Write("Введіть перше число: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введіть друге число: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введіть третэ число: ");
+                int num3 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введіть четверте число: ");
+                int num4 = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Результат: {(num1 * 1000)+(num2 * 100)+(num3 * 10)+(num4)}");
             }
             catch (Exception ex)
             {
