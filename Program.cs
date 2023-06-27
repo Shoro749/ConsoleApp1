324ed5a6868e61ff9f1effd6bfab41089b60ee95
Shoro749 <romanbro749@gmail.com>
2023-06-27 13:04:12 +0300

    Task 3 completed

--- Program.cs
+++ Program.cs
@@ -8,35 +8,28 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Користувач з клавіатури вводить шестизначне число.
+                Необхідно перевернути число і відобразити результат.
+                Наприклад, якщо введено 341256, результат 652143.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
+                Console.Write("Введіть шестизначне число: ");
                 int num = Convert.ToInt32(Console.ReadLine());
-
-                if (num > 0 && num < 100)
-                {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
                 
+                if (num < 100000 && num > 999999) 
+                {
+                    throw new Exception("Число повинно бути шестизначним!");
+                }
+
+                string sNum = num.ToString();
+                int[] numbers = new int[6];
+
+                for (int i = sNum.Length-1; i > -1; i--)
+                {
+                    Console.Write(sNum[i]);
+                }
             }
             catch (Exception ex)
             {
