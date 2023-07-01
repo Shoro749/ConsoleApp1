04cd37b511c8f00cd90e15d5b888de1d89d14e43
Shoro749 <romanbro749@gmail.com>
2023-07-01 17:22:46 +0300

    Task 7 completed

--- Program.cs
+++ Program.cs
@@ -1,42 +1,44 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
+        public static string ReverseStr(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
         static void Main(string[] args)
         {
             try
             {
+                /*Користувач вводить речення з клавіатури. Вам необхідно перевернути кожне слово речення і вивести результат
+                на екрані.
+                Наприклад:
+                користувач ввів: sun cat dogs cup tea;
+                після перевороту: nus tac sgod puc aet.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введіть речення: ");
+                string ?str = Convert.ToString(Console.ReadLine());
+                str = str.Trim();
+                string[] words = str.Split(' ');
 
-                if (num > 0 && num < 100)
+                for (int i = 0; i < words.Length; i++)
                 {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
+                    words[i] = ReverseStr(words[i]);
                 }
-                else
+
+                foreach (string word in words)
                 {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
+                    Console.WriteLine(word);
                 }
-                
             }
             catch (Exception ex)
             {
