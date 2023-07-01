9104f92f3b5f9c321ea733377bef8a6dfa9b9d17
Shoro749 <romanbro749@gmail.com>
2023-07-01 15:44:35 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -8,35 +8,15 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Дано 2 масиви розмірності M і N відповідно.
+                Необхідно переписати до третього масиву загальні
+                елементи перших двох масивів без повторень.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
                 Console.Write("Введіть число: ");
                 int num = Convert.ToInt32(Console.ReadLine());
-
-                if (num > 0 && num < 100)
-                {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
