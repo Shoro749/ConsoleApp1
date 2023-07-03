a5e8041336213f7a58a522b92ee7c462cbfe2305
Shoro749 <romanbro749@gmail.com>
2023-07-03 13:47:10 +0300

    Task 9 completed

--- Program.cs
+++ Program.cs
@@ -8,35 +8,25 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть додаток для підрахунку кількості входжень
+                підрядка в рядок. Користувач вводить вихідний рядок і
+                слово для пошуку. Додаток відображає результат пошуку.
+                Наприклад:
+                користувач ввів: Why she had to go. I don't know, she
+                wouldn't say;
+                підрядок для пошуку: she;
+                результат пошуку: 2.*/
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введіть речення: ");
+                string ?str = Convert.ToString(Console.ReadLine());
+                Console.Write("Введіть підрядок для пошуку: ");
+                string? str2 = Convert.ToString(Console.ReadLine());
 
-                if (num > 0 && num < 100)
-                {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
+                str = str.Trim();
+                string[] separation = str.Split(str2);
+                Console.Write($"Результат пошуку: {separation.Length - 1}");
             }
             catch (Exception ex)
             {
