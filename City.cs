c7347efa30022d9f1f897cbd6597f31f036e60b4
Shoro749 <romanbro749@gmail.com>
2023-07-08 16:45:24 +0300

    Task 4 completed

---
+++ City.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть клас «Місто». Необхідно зберігати у полях
+класу: назву міста, назву країни, кількість жителів у місті,
+телефонний код міста, назву районів міста. Реалізуйте методи класу для введення даних, виведення даних. Реалізуйте
+доступ до окремих полів через методи класу.*/
+
+namespace ConsoleApp1
+{
+    internal class City
+    {
+        private string CityName;
+        private string Country;
+        private int Number;
+        private int TelCode;
+        private string[] districts = new string[5];
+
+        public City() 
+        {
+            CityName = "NoName";
+            Country = "NoName";
+            Number = 0;
+            TelCode = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                districts[i] = "NoName";
+            }
+        }
+
+        public City(string city, string contry, int num, int tCode, string[] arr)
+        {
+            CityName = city;
+            Country = contry;
+            Number = num;
+            TelCode = tCode;
+            for (int i = 0; i < 5; i++)
+            {
+                districts[i] = arr[i];
+            }
+        }
+
+        public string CityNameInfo
+        {
+            get { return CityName; }
+            set { CityName = value; }
+        }
+
+        public string CountryNameInfo
+        {
+            get { return Country; }
+            set { Country = value; }
+        }
+
+        public int CityNumberInfo
+        {
+            get { return Number; }
+            set { Number = value; }
+        }
+
+        public int TelCodeInfo
+        {
+            get { return TelCode; }
+            set { TelCode = value; }
+        }
+
+        public string[] DistrictsInfo
+        {
+            get { return districts; }
+            set { districts = value; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,143 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть клас «Місто». Необхідно зберігати у полях
+                класу: назву міста, назву країни, кількість жителів у місті,
+                телефонний код міста, назву районів міста. Реалізуйте методи класу для введення даних, виведення даних. Реалізуйте
+                доступ до окремих полів через методи класу.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
+                Console.Write("Введіть назву міста: ");
+                string ?city = Convert.ToString(Console.ReadLine());
+                Console.Write("Введіть назву країни: ");
+                string ?country = Convert.ToString(Console.ReadLine());
+                Console.Write("Введіть число населення міста: ");
                 int num = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введіть телефоний код міста: ");
+                int telNum = Convert.ToInt32(Console.ReadLine());
 
-                if (num > 0 && num < 100)
+                string[] arr = new string[5];
+
+                for (int i = 0; i < 5; i++)
                 {
-                    if (num % 3 == 0)
+                    Console.Write($"{i+1}) Введіть назви районів: ");
+                    arr[i] = Convert.ToString(Console.ReadLine());
+                }
+
+                var v = new City(city, country, num, telNum, arr);
+
+                Console.WriteLine("\n");
+
+                while (true)
+                {
+                    int select = 0;
+                    Console.WriteLine("1) Ввести назву міста");
+                    Console.WriteLine("2) Ввести назву країни");
+                    Console.WriteLine("3) Ввести число населення");
+                    Console.WriteLine("4) Ввести телефонний код міста");
+                    Console.WriteLine("5) Ввести назви районів міста");
+                    Console.WriteLine("6) Дальше");
+                    Console.Write("Введіть вибір: ");
+                    select = Convert.ToInt32(Console.ReadLine());
+
+                    if (select == 1)
                     {
-                        Console.WriteLine("Fizz");
+                        Console.Write("Введіть нову назву міста: ");
+                        v.CityNameInfo = Convert.ToString(Console.ReadLine());
                     }
-                    else
+
+                    if (select == 2)
                     {
-                        if (num % 5 == 0)
+                        Console.Write("Введіть нову назву країни: ");
+                        v.CountryNameInfo = Convert.ToString(Console.ReadLine());
+                    }
+
+                    if (select == 3)
+                    {
+                        Console.Write("Введіть нове число населення: ");
+                        v.CityNumberInfo = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    if (select == 4)
+                    {
+                        Console.Write("Введіть новий телефонний код: ");
+                        v.TelCodeInfo = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    if(select == 5)
+                    {
+                        for (int i = 0; i < 5; i++) 
                         {
-                            Console.WriteLine("Buzz");
+                            arr[i] = Convert.ToString(Console.ReadLine());
                         }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        v.DistrictsInfo = arr;
+                    }
+
+                    if (select == 6)
+                    {
+                        break;
+                    }
+
+                    if (select < 1 && select > 6)
+                    {
+                        throw new Exception("Неправилний вибір");
                     }
                 }
-                else
+
+                Console.WriteLine("\n");
+
+                while (true)
                 {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
+                    int select = 0;
+                    Console.WriteLine("1) Вивести назву міста");
+                    Console.WriteLine("2) Вивести назву країни");
+                    Console.WriteLine("3) Вивести число населення");
+                    Console.WriteLine("4) Вивести телефонний код міста");
+                    Console.WriteLine("5) Вивести назви районів міста");
+                    Console.WriteLine("6) Вийти");
+                    Console.Write("Введіть вибір: ");
+                    select = Convert.ToInt32(Console.ReadLine());
+
+                    if (select == 1)
+                    {
+                        Console.Write($"Назва міста: {v.CityNameInfo}\n");
+                    }
+
+                    if (select == 2)
+                    {
+                        Console.Write($"Назву країни: {v.CountryNameInfo}\n");
+                    }
+
+                    if (select == 3)
+                    {
+                        Console.Write($"Число населення: {v.CityNumberInfo}\n");
+                    }
+
+                    if (select == 4)
+                    {
+                        Console.Write($"Введіть новий телефонний код: {v.TelCodeInfo} \n");
+                    }
+
+                    if (select == 5)
+                    {
+                        foreach (var item in v.DistrictsInfo)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+
+                    if (select == 6)
+                    {
+                        break;
+                    }
+                    
+                    if (select < 1 && select > 6)
+                    {
+                        throw new Exception("Неправилний вибір");
+                    }
                 }
-                
             }
             catch (Exception ex)
             {
