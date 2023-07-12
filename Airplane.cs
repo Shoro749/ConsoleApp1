8dcb18d97fe5d7350afb3a882059c857fcb19370
Shoro749 <romanbro749@gmail.com>
2023-07-12 13:49:38 +0300

    Task 6 completed

---
+++ Airplane.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*/*Реалізуйте клас «Літак». Необхідно зберігати у полях
+класу: назву літака, назву виробника, рік випуску, тип літака. Реалізуйте конструктори і методи класу для введення
+і виведення даних. Реалізуйте доступ до окремих полів
+через методи класу. Використовуйте механізм навантаження методів.*/
+
+
+namespace ConsoleApp1
+{
+    internal class Airplane
+    {
+        private string _name;
+        private string _manufacturerName;
+        private int _graduationYear;
+        private string _type;
+
+        public Airplane()
+        {
+            _name = "Noname";
+            _manufacturerName = "Noname";
+            _graduationYear = 0000;
+            _type = "Notype";
+        }
+
+        public Airplane(string name, string manufacturerName, int graduationYear, string type)
+        {
+            _name = name;
+            _manufacturerName = manufacturerName;
+            _graduationYear = graduationYear;
+            _type = type;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                  else
+                    _name = value; }
+        }
+
+        public string ManufacturerName
+        {
+            get { return _manufacturerName; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _manufacturerName = value;
+            }
+        }
+
+        public int GraduationYear
+        {
+            get { return _graduationYear; }
+            set
+            {
+                if (value < 1000 || value > 2023)
+                    throw new Exception("Введено неправильний рік!");
+                else
+                    _graduationYear = value;
+            }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _type = value;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,65 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Реалізуйте клас «Літак». Необхідно зберігати у полях
+                класу: назву літака, назву виробника, рік випуску, тип літака. Реалізуйте конструктори і методи класу для введення
+                і виведення даних. Реалізуйте доступ до окремих полів
+                через методи класу. Використовуйте механізм навантаження методів.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                var v = new Airplane("Eagle", "AirplaneFactory", 2020, "Passenger's");
 
-                if (num > 0 && num < 100)
+                while (true)
                 {
-                    if (num % 3 == 0)
+                    bool Exit = false;
+                    Console.WriteLine("1) Змінити назву");
+                    Console.WriteLine("2) Змінити назву виробника");
+                    Console.WriteLine("3) Змінити рік випуску");
+                    Console.WriteLine("4) Змінити тип літака");
+                    Console.WriteLine("5) Показати інформацію про літак");
+                    Console.WriteLine("6) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            Console.Write("\nВведіть нову назву: ");
+                            v.Name = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 2:
+                            Console.Write("\nВведіть новий рік випуску: ");
+                            v.ManufacturerName = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.Write("\nВведіть нову назву виробника: ");
+                            v.GraduationYear = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 4:
+                            Console.Write("\nВведіть новий тип літака: ");
+                            v.Type = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 5:
+                            Console.WriteLine($"\nНазва літака: {v.Name}\nНазва виробника: {v.ManufacturerName}" +
+                                $"\nРік випуску: {v.GraduationYear}\nТип літака: {v.Type}\n");
+                            break;
+
+                        case 6:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
