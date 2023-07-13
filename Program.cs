5caf5a632ce8927297d8bc8942ec78582fb9ca38
Shoro749 <romanbro749@gmail.com>
2023-07-13 14:35:04 +0300

    Task 4 completed

--- Program.cs
+++ Program.cs
@@ -8,35 +8,65 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть клас «Веб-сайт». Необхідно зберігати у
+                полях класу: назву сайту, шлях до сайту, опис сайту,
+                ip-адресу сайту. Реалізуйте методи класу для введення
+                даних, виведення даних. Реалізуйте доступ до окремих
+                полів через методи класу.*/
+
+                var v = new Website("YouTube", "www.youtube.com", "Website for watching video!", "8.8.8.8");
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    bool Exit = false;
+                    Console.WriteLine("\n1) Змінити назву сайта");
+                    Console.WriteLine("2) Змінити шлях до сайту");
+                    Console.WriteLine("3) Змінити опис сайту");
+                    Console.WriteLine("4) Змінити ip-адресу сайту");
+                    Console.WriteLine("5) Показати інформацію про сайт");
+                    Console.WriteLine("6) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
 
-                if (num > 0 && num < 100)
-                {
-                    if (num % 3 == 0)
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            Console.Write("\nВведіть нову назву сайту: ");
+                            v.Name = Convert.ToString(Console.ReadLine()); 
+                            break;
+
+                        case 2:
+                            Console.Write("\nВведіть новий шлях до сайту: ");
+                            v.Way = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.Write("\nВведіть новий опис сайту: ");
+                            v.Description = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 4:
+                            Console.Write("\nВведіть нову ip-адресу сайту: ");
+                            v.IP = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 5:
+                            v.ShowInfo();
+                            break;
+
+                        case 6:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
---
+++ Website.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть клас «Веб-сайт». Необхідно зберігати у
+полях класу: назву сайту, шлях до сайту, опис сайту,
+ip-адресу сайту. Реалізуйте методи класу для введення
+даних, виведення даних. Реалізуйте доступ до окремих
+полів через методи класу.*/
+
+namespace ConsoleApp1
+{
+    internal class Website
+    {
+        private string _name;
+        private string _way;
+        private string _description;
+        private string _ip;
+
+        public Website()
+        {
+            _name = "Noname";
+            _way = "Nope";
+            _description = "No_description";
+            _ip = "000.0.0.0";
+        }
+
+        public Website(string name, string way, string description, string ip)
+        {
+            _name = name;
+            _way = way;
+            _description = description;
+            _ip = ip;
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _name = value;
+            }
+        }
+
+        public string Way
+        {
+            get { return _way; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _way = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _description = value;
+            }
+        }
+
+        public string IP
+        {
+            get { return _ip; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _ip = value;
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\nНазва сайту: {Name}\nШлях до сайту: {Way}\n" +
+                $"Опис: {Description}\nIp-адреса: {IP}\n");
+        }
+    }
+}
