12ab4fd6ac8b5f7cb4555b3e4835ad9d642901ab
Shoro749 <romanbro749@gmail.com>
2023-07-13 15:24:47 +0300

    Task 5 completed

---
+++ Magazine.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть клас «Журнал». Необхідно зберігати у полях
+класу: назву журналу, рік заснування, опис журналу,
+контактний телефон, email. Реалізуйте методи класу для
+введення даних, виведення даних. Реалізуйте доступ до
+окремих полів через методи класу.*/
+
+namespace ConsoleApp1
+{
+    internal class Magazine
+    {
+        private string _name;
+        private int _year;
+        private string _description;
+        private string _telephone;
+        private string _email;
+
+        public Magazine()
+        {
+            _name = "Noname";
+            _year = 0000;
+            _description = "No_description";
+            _telephone = "+3800000000000";
+            _email = "No_email";
+        }
+        public Magazine(string name, int year, string description, string tel, string email)
+        {
+            _name = name;
+            _year = year;
+            _description = description;
+            _telephone = "+380" + tel;
+            _email = email;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _name = value;
+            }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                if (value < 1000 || value > 9999)
+                    throw new Exception("Введено неправильний рік!");
+                else
+                    _year = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _description = value;
+            }
+        }
+
+        public string Telephone
+        {
+            get { return _telephone; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _telephone = "+380" + value;
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _email = value;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,72 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть клас «Журнал». Необхідно зберігати у полях
+                класу: назву журналу, рік заснування, опис журналу,
+                контактний телефон, email. Реалізуйте методи класу для
+                введення даних, виведення даних. Реалізуйте доступ до
+                окремих полів через методи класу.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                var v = new Magazine("GoodMorning", 2016, "Цей журнал розповідає про новини в світі!", "0986782166", "goodmorning@gmail.com");
 
-                if (num > 0 && num < 100)
+                while (true)
                 {
-                    if (num % 3 == 0)
+                    bool Exit = false;
+                    Console.WriteLine("\n1) Змінити назву журналу");
+                    Console.WriteLine("2) Змінити рік випуску");
+                    Console.WriteLine("3) Змінити опис журналу");
+                    Console.WriteLine("4) Змінити контактний номер журналу");
+                    Console.WriteLine("5) Змінити еmail журналу");
+                    Console.WriteLine("6) Показати інформацію про журнал");
+                    Console.WriteLine("7) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            Console.Write("\nВведіть нову назву журналу: ");
+                            v.Name = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 2:
+                            Console.Write("\nВведіть новий рік випуску: ");
+                            v.Year = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.Write("\nВведіть новий опис журналу: ");
+                            v.Description = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 4:
+                            Console.Write("\nВведіть новий контактний номер: ");
+                            v.Telephone = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 5:
+                            Console.Write("\nВведіть новий еmail: ");
+                            v.Email = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 6:
+                            Console.WriteLine($"\nНазва журналу: {v.Name}\nРік випуску: {v.Year}" +
+                                $"\nОпис: {v.Description}\nКонтактний номер: {v.Telephone}\nЕmail: {v.Email}\n");
+                            break;
+
+                        case 7:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
