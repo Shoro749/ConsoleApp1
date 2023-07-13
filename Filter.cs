6372773c0d61a67ee5350434bc6d0d8397719743
Shoro749 <romanbro749@gmail.com>
2023-07-13 14:10:06 +0300

    Task 3 completed

---
+++ Filter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Напишіть метод, який фільтрує масив за переданими
+параметрами. Метод приймає параметри: оригінальний_масив, масив_з_даними_для_фільтрації.
+Метод повертає оригінальний масив без елементів,
+які знаходяться у масиві для фільтрації.
+Наприклад:
+1 2 6 -1 88 7 6 — оригінальний масив
+6 88 7 — масив для фільтрації
+1 2 -1 — результат роботи методу*/
+
+namespace ConsoleApp1
+{
+    internal class Filter
+    {
+        public void filter(int[] arr, int[] filter) 
+        {
+            int[] newArr = new int[arr.Length];
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool check = true;
+                for (int j = 0; j < filter.Length; j++)
+                {
+                    if (arr[i] == filter[j])
+                    {
+                        check = false;
+                    }
+                }
+                if (check)
+                {
+                    newArr[temp] = arr[i];
+                    temp++;
+                }
+            }
+
+            foreach (var item in newArr) 
+            {
+                if (item > 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,40 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Напишіть метод, який фільтрує масив за переданими
+                параметрами. Метод приймає параметри: оригінальний_масив, масив_з_даними_для_фільтрації.
+                Метод повертає оригінальний масив без елементів,
+                які знаходяться у масиві для фільтрації.
+                Наприклад:
+                1 2 6 -1 88 7 6 — оригінальний масив
+                6 88 7 — масив для фільтрації
+                1 2 -1 — результат роботи методу*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
+                Random rnd = new Random();
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[8];
+                int[] filter = new int[3];
 
-                if (num > 0 && num < 100)
+                for (int i = 0; i < arr.Length; i++)
                 {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
+                    arr[i] = rnd.Next(0, 100);
+                    Console.Write(arr[i] + " ");
                 }
-                else
+
+                Console.Write("\n");
+
+                for (int i = 0; i < filter.Length; i++) 
                 {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
+                    Console.Write($"\n{i+1}) Введіть число для фільтра: ");
+                    filter[i] = Convert.ToInt32(Console.ReadLine());
                 }
-                
+
+                Console.Write("\n");
+
+                var v = new Filter();
+                v.filter(arr, filter);
             }
             catch (Exception ex)
             {
