663489d47eb5634beecf41c6149c5928911ce577
Shoro749 <romanbro749@gmail.com>
2023-07-13 15:40:16 +0300

    Task 6 completed

--- Program.cs
+++ Program.cs
@@ -8,35 +8,72 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть клас «Магазин». Необхідно зберігати у полях
+                класу: назву магазину, адреса, опис профілю магазину,
+                контактний телефон, email. Реалізуйте методи класу для
+                введення даних, виведення даних. Реалізуйте доступ до
+                окремих полів через методи класу.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                var v = new Shop("ATБ", "s.Taras`s Shevchenko`s", "Тут продається все", 0986782166, "АТБ@gmail.com");
 
-                if (num > 0 && num < 100)
+                while (true)
                 {
-                    if (num % 3 == 0)
+                    bool Exit = false;
+                    Console.WriteLine("\n1) Змінити назву магазину");
+                    Console.WriteLine("2) Змінити адресу");
+                    Console.WriteLine("3) Змінити опис профілю магазину");
+                    Console.WriteLine("4) Змінити контактний номер магазину");
+                    Console.WriteLine("5) Змінити еmail магазина");
+                    Console.WriteLine("6) Показати інформацію про магазин");
+                    Console.WriteLine("7) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            Console.Write("\nВведіть нову назву магазина: ");
+                            v.Name = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 2:
+                            Console.Write("\nВведіть нову адресу: ");
+                            v.Address = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.Write("\nВведіть новий опис магазина: ");
+                            v.Description = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 4:
+                            Console.Write("\nВведіть новий контактний номер: +380");
+                            v.Telephone = Convert.ToInt64(Console.ReadLine());
+                            break;
+
+                        case 5:
+                            Console.Write("\nВведіть новий еmail: ");
+                            v.Email = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 6:
+                            Console.WriteLine($"\nНазва журналу: {v.Name}\nРік випуску: {v.Address}" +
+                                $"\nОпис: {v.Description}\nКонтактний номер: {v.Telephone}\nЕmail: {v.Email}\n");
+                            break;
+
+                        case 7:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
---
+++ Shop.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть клас «Магазин». Необхідно зберігати у полях
+класу: назву магазину, адреса, опис профілю магазину,
+контактний телефон, email. Реалізуйте методи класу для
+введення даних, виведення даних. Реалізуйте доступ до
+окремих полів через методи класу.*/
+
+namespace ConsoleApp1
+{
+    internal class Shop
+    {
+        private string _name;
+        private string _address;
+        private string _description;
+        private long _tel;
+        private string _email;
+
+        public Shop() 
+        {
+            _name = "Noname";
+            _address = "No_Address";
+            _description = "No_description";
+            _tel = 0;
+            _email = "No_email";
+        }
+        public Shop(string name, string address, string descripction, long telephone, string email)
+        {
+            _name = name;
+            _address = address;
+            _description = descripction;
+            _tel = telephone;
+            _email = email;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _name = value;
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _address = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _description = value;
+            }
+        }
+
+        public long Telephone
+        {
+            get { return _tel; }
+            set
+            {
+                if (value < 1000000000 || value > 9999999999)
+                    throw new Exception("Введено неправильний номер!");
+                else
+                    _tel = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _email = value;
+            }
+        }
+    }
+}
