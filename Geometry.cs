71dac7eb664b7cb135227e1b9dec15525cda88a3
Shoro749 <romanbro749@gmail.com>
2023-07-15 17:22:19 +0300

    gfh

---
+++ Geometry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть класи для відображення різних геометричних
+фігур: трикутника, прямокутника, квадрата. Використовуйте механізми просторів імен.*/
+
+namespace Triangle
+{
+    public class Geometry
+    {
+        public void ShowFigure()
+        {
+            Console.WriteLine("\t\t\t\t\t\t\t\t*");
+            Console.WriteLine("\t\t\t\t\t\t\t**");
+            Console.WriteLine("\t\t\t\t\t\t****");
+            Console.WriteLine("\t\t\t\t\t******");
+            Console.WriteLine("\t\t\t\t********");
+            Console.WriteLine("\t\t\t**********");
+            Console.WriteLine("\t\t************");
+            Console.WriteLine("\t**************");
+            Console.WriteLine("****************");
+        }
+    }
+}
+
+namespace Rectangle
+{
+    public class Geometry
+    {
+    }
+}
+
+namespace Square
+{
+    public class Geometry
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,13 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть класи для відображення різних геометричних
+                фігур: трикутника, прямокутника, квадрата. Використовуйте механізми просторів імен.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
-
-                if (num > 0 && num < 100)
-                {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
+                Triangle.Geometry figure = new Triangle.Geometry();
             }
             catch (Exception ex)
             {
