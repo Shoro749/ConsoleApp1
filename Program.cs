8ad9d6761bf2a992ac0f6ae659e6902f689f3170
Shoro749 <romanbro749@gmail.com>
2023-07-19 19:36:17 +0300

    Task 2 completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp1
 {
@@ -8,35 +9,38 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть додаток для перекладу звичайного тексту в
+                азбуку Морзе. Користувач вводить текст. Додаток виводить
+                введений текст азбукою Морзе. Використовуйте механізми
+                просторів імен.*/
+
+                string[] morze = new string[26] { "*-", "-***", "-*-*", "-**", "*", "**-*", "--*",
+                    "****", "**", "*---", "-*-", "*-**", "--", "-*", "---", "*--*", "--*-", "*-*",
+                    "***", "-", "**-", "***-", "*--", "-**-", "-*--", "--**"};
+                string[] abc = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",  "w", "x", "y", "z",};
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                Console.Write("1) Текст -> морзе\n2) Морзе -> текст\nВведіть свій вибір: ");
+                int select = Convert.ToInt32(Console.ReadLine()); 
+                switch(select)
+                {
+                    case 1:
+                        Console.Write("Введіть текст(eng): ");
+                        string? str = Convert.ToString(Console.ReadLine());
+                        ToMorze.Translator translator = new ToMorze.Translator();
+                        translator.translating(str, morze, abc);
+                        break;
 
-                if (num > 0 && num < 100)
-                {
-                    if (num % 3 == 0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
-                    }
+                    case 2:
+                        Console.Write("Введіть текст: ");
+                        str = Convert.ToString(Console.ReadLine());
+                        ToAbc.Translator _translator = new ToAbc.Translator();
+                        _translator.translating(str, morze, abc);
+                        break;
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
---
+++ Translator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToMorze
+{
+    public class Translator
+    {
+        public void translating(string text, string[] morze, string[] abc) 
+        {
+            string[] NewText = new string[text.Length];
+            int temp = -1;
+            string str = char.ToLower(text[0]) + text.Substring(1);
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < morze.Length; j++)
+                {
+                    if (Convert.ToString(str[i]) == abc[j])
+                    {
+                        temp++;
+                        NewText[temp] = morze[j];
+                        break;
+                    }
+                }
+            }
+
+            foreach (var item in NewText)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
+
+namespace ToAbc
+{
+    public class Translator
+    {
+        public void translating(string text, string[] morze, string[] abc)
+        {
+            string[] NewText = new string[text.Length];
+            int temp = -1;
+            string[] str = text.Split(' ');
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < morze.Length; j++)
+                {
+                    if (Convert.ToString(str[i]) == morze[j])
+                    {
+                        temp++;
+                        NewText[temp] = abc[j];
+                        break;
+                    }
+                }
+            }
+
+            foreach (var item in NewText)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
