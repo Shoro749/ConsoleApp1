179d869514e5b91c431c0d863f9b30352a36db82
Shoro749 <romanbro749@gmail.com>
2023-07-22 17:32:32 +0300

    Task 1 completed

---
+++ Employee.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+        private string NSM;
+        private string birthday;
+        private int telephone;
+        private string email;
+        private string position;
+        private string JobDescription;
+        private int salary;
+        public Employee()
+        {
+            NSM = "NoName";
+            birthday = "00.00.0000";
+            telephone = 0;
+            email = "NULL";
+            position = "NoPosition";
+            JobDescription = "NULL";
+            salary = 0;
+        }
+        public Employee(string nSM, string birthday, int telephone, string email, string position, string jobDescription, int salary)
+        {
+            NSM = nSM;
+            this.birthday = birthday;
+            this.telephone = telephone;
+            this.email = email;
+            this.position = position;
+            JobDescription = jobDescription;
+            this.salary = salary;
+        }
+        public void getInfo()
+        {
+            Console.WriteLine($"\nПІБ: {NSM}\nДата народження: {birthday}\nНомер телефону: {telephone}\n" +
+                $"Емейл: {email}\nПосада: {position}\nОпис службових обов’язків:{JobDescription}\n" +
+                $"Зарплата: {salary}\n");
+        }
+
+        public string NSM_ 
+        {
+            get { return NSM; }
+            set { NSM = value; }
+        }
+        public void setBirthday(string birthday) { this.birthday = birthday; }
+        public void setTel(int tel) { telephone = tel; }
+        public void setEmail(string email) { this.email = email; }
+        public void setPosition(string position) { this.position = position; }
+        public void setJobDescription(string JobDescription) { this.JobDescription = JobDescription; }
+        public int Salary {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public static int operator+(Employee salary, int sum)
+        {
+            return salary + sum;
+        }
+        public static int operator -(Employee salary, int sum)
+        {
+            return salary - sum;
+        }
+        public static bool operator ==(Employee v, Employee q)
+        {
+            return v.Salary == q.Salary;
+        }
+        public static bool operator !=(Employee v, Employee q)
+        {
+            return !(v == q);
+        }
+        public static bool operator >(Employee v, Employee q)
+        {
+            return v > q;
+        }
+        public static bool operator <(Employee v, Employee q)
+        {
+            return v < q;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,125 @@ namespace ConsoleApp1
         {
             try
             {
+                /*В одному з попередніх практичних завдань ви створювали клас «Співробітник». Додайте до вже створеного класу
+                інформацію про заробітну плату працівника. Виконайте
+                навантаження + (для збільшення зарплати на вказану кількість), – (для зменшення зарплати на вказану кількість), ==
+                (перевірка на рівність зарплат працівників), < і > (перевірка
+                на меншу чи більшу кількість зарплат працівників), != і
+                Equals. Використовуйте механізм властивостей полів класу.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                var v = new Employee("ПІБ", "2000", 0981234567, "ПІБ@gmail.com", "Cleaner", "Прибирання", 10000);
+                var q = new Employee("Bob", "2001", 0987654321, "Bob@gmail.com", "Programmer", "Писати код", 25000);
 
-                if (num > 0 && num < 100)
+                while (true)
                 {
-                    if (num % 3 == 0)
+                    bool Exit = false;
+                    int select;
+                    Console.WriteLine($"\n1) Вивести інформацію");
+                    Console.WriteLine($"2) Змінити ПІБ");
+                    Console.WriteLine($"3) Змінити дату народження");
+                    Console.WriteLine($"4) Змінити номер телефону");
+                    Console.WriteLine($"5) Змінити емейл");
+                    Console.WriteLine($"6) Змінити посаду");
+                    Console.WriteLine($"7) Змінити опис службових обов’язків");
+                    Console.WriteLine($"8) Змінити зарплату працівника");
+                    Console.WriteLine($"9) Перевіриити зарплати працівників");
+                    Console.WriteLine($"10) Вихід");
+                    Console.Write("Введіть вибір: ");
+                    select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            v.getInfo();
+                            q.getInfo();
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Введіть ПІБ:");
+                            v.NSM_ = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Введіть нову дату народження:");
+                            string birthday = Convert.ToString(Console.ReadLine());
+                            v.setBirthday(birthday);
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Введіть новий номер телефону:");
+                            int tel = Convert.ToInt32(Console.ReadLine());
+                            v.setTel(tel);
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Введіть новий емейл:");
+                            string email = Convert.ToString(Console.ReadLine());
+                            v.setEmail(email);
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Введіть нову посаду:");
+                            string position = Convert.ToString(Console.ReadLine());
+                            v.setPosition(position);
+                            break;
+
+                        case 7:
+                            Console.WriteLine("Введіть новий опис службових обов’язків:");
+                            string JobDescription = Convert.ToString(Console.ReadLine());
+                            v.setJobDescription(JobDescription);
+                            break;
+
+                        case 8:
+                            Console.Write("\nВведіть + щоб підвищити зарплату, або - зменшити: ");
+                            string _select = Convert.ToString(Console.ReadLine());
+                            if (_select == "+")
+                            {
+                                Console.Write("\nВведіть на скільки збільшити зарплату: ");
+                                int sum = Convert.ToInt32(Console.ReadLine());
+                                v.Salary = v.Salary + sum;
+                            }
+                            else if (_select == "-")
+                            {
+                                Console.Write("\nВведіть на скільки зменшити зарплату: ");
+                                int sum = Convert.ToInt32(Console.ReadLine());
+                                v.Salary = v.Salary - sum;
+                            }
+                            else
+                            {
+                                throw new Exception("Неправильний вибір!");
+                            }
+                            break;
+
+                        case 9:
+                            Console.Write("\nВведіть 1 щоб перевірити зарплату працівників на рівність,\nабо 2 більше/менше: ");
+                            select = Convert.ToInt32(Console.ReadLine());
+                            if (select == 1)
+                            {
+                                if (v.Salary == q.Salary) { Console.WriteLine("\nЗарплата однакова!"); }
+                                else { Console.WriteLine("\nЗарплата не однакова!"); }
+                            }
+                            else if (select == 2)
+                            {
+                                if (v.Salary > q.Salary) { Console.WriteLine($"\nЗарплата {q.NSM_} менша!"); }
+                                else { Console.WriteLine($"\nЗарплата {q.NSM_} більша!"); }
+                            }
+                            else { throw new Exception("Неправильний вибір!"); }
+                            break;
+
+                        case 10:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit == true)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
