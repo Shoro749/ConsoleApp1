54a5883676225b7b5072b21961dd8f36ee9052bf
Shoro749 <romanbro749@gmail.com>
2023-07-28 18:01:37 +0300

    Task 2 completed

--- Program.cs
+++ Program.cs
@@ -8,35 +8,112 @@ namespace ConsoleApp1
         {
             try
             {
+                /*В одному з попередніх практичних завдань ви створювали клас «Магазин». Додайте до вже створеного класу
+                інформацію про площу магазину. Виконайте навантаження + (для збільшення площі магазину на вказаний
+                розмір), — (для зменшення площі магазину на вказаний
+                розмір), == (перевірка на рівність площ магазинів), < і >
+                (перевірка магазинів менших або більших за площею),
+                != і Equals. Використовуйте механізм властивостей
+                полів класу.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                Shop q = new Shop("Сільпо", "s.Street", "Тут продається все", 0981111111, "silpo@gmail.com", 60);
+                Shop v = new Shop("ATБ", "s.Taras`s Shevchenko`s", "Тут продається все", 0986782166, "АТБ@gmail.com", 75);
 
-                if (num > 0 && num < 100)
+                while (true)
                 {
-                    if (num % 3 == 0)
+                    bool Exit = false;
+                    Console.WriteLine("\n1) Змінити назву магазину");
+                    Console.WriteLine("2) Змінити адресу");
+                    Console.WriteLine("3) Змінити опис профілю магазину");
+                    Console.WriteLine("4) Змінити контактний номер магазину");
+                    Console.WriteLine("5) Змінити еmail магазина");
+                    Console.WriteLine("6) Змінити площу магазину");
+                    Console.WriteLine("7) Порівняти площі магазинів");
+                    Console.WriteLine("8) Показати інформацію про магазин");
+                    Console.WriteLine("9) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            Console.Write("\nВведіть нову назву магазина: ");
+                            v.Name = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 2:
+                            Console.Write("\nВведіть нову адресу: ");
+                            v.Address = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.Write("\nВведіть новий опис магазина: ");
+                            v.Description = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 4:
+                            Console.Write("\nВведіть новий контактний номер: +380");
+                            v.Telephone = Convert.ToInt64(Console.ReadLine());
+                            break;
+
+                        case 5:
+                            Console.Write("\nВведіть новий еmail: ");
+                            v.Email = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 6:
+                            Console.Write("\nВведіть 1 щоб збільшити, або 2 щоб зменшити площу: ");
+                            select = Convert.ToInt32(Console.ReadLine());
+                            int num;
+
+                            if (select == 1)
+                            {
+                                Console.Write("\nВведіть число: ");
+                                num = Convert.ToInt32(Console.ReadLine());
+                                v.Area = v.Area + num;
+                            }
+                            else
+                            {
+                                Console.Write("\nВведіть число: ");
+                                num = Convert.ToInt32(Console.ReadLine());
+                                v.Area = v.Area - num;
+                            }
+                            break;
+
+                        case 7:
+                            if (v.Area == q.Area)
+                            {
+                                Console.WriteLine($"\nПлоща магазину {v.Name} дорівнює площі магазину {q.Name}!");
+                            }
+                            else if (v.Area > q.Area)
+                            {
+                                Console.WriteLine($"\nПлоща магазину {v.Name} більша ніж площа магазину {q.Name}!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"\nПлоща магазину {v.Name} менша ніж площа магазину {q.Name}!");
+                            }
+                            break;
+
+                        case 8:
+                            Console.WriteLine($"\nНазва журналу: {v.Name}\nРік випуску: {v.Address}" +
+                                $"\nОпис: {v.Description}\nКонтактний номер: {v.Telephone}\nЕmail: {v.Email}\n" +
+                                $"Площа: {v.Area}\n");
+                            break;
+
+                        case 9:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
---
+++ Shop.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*В одному з попередніх практичних завдань ви створювали клас «Магазин». Додайте до вже створеного класу
+інформацію про площу магазину. Виконайте навантаження + (для збільшення площі магазину на вказаний
+розмір), — (для зменшення площі магазину на вказаний
+розмір), == (перевірка на рівність площ магазинів), < і >
+(перевірка магазинів менших або більших за площею),
+!= і Equals. Використовуйте механізм властивостей
+полів класу.*/
+
+namespace ConsoleApp1
+{
+    internal class Shop
+    {
+        private string _name;
+        private string _address;
+        private string _description;
+        private long _tel;
+        private string _email;
+        private int _area;
+
+        public Shop()
+        {
+            _name = "Noname";
+            _address = "No_Address";
+            _description = "No_description";
+            _tel = 0;
+            _email = "No_email";
+            _area = 0;
+        }
+        public Shop(string name, string address, string descripction, long telephone, string email, int area)
+        {
+            _name = name;
+            _address = address;
+            _description = descripction;
+            _tel = telephone;
+            _email = email;
+            _area = area;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _name = value;
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _address = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _description = value;
+            }
+        }
+
+        public long Telephone
+        {
+            get { return _tel; }
+            set
+            {
+                if (value < 1000000000 || value > 9999999999)
+                    throw new Exception("Введено неправильний номер!");
+                else
+                    _tel = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _email = value;
+            }
+        }
+
+        public int Area
+        {
+            get { return _area; }
+            set { _area = value; }
+        }
+
+        public static int operator +(Shop v, int num) { return v.Area + num; }
+        public static int operator -(Shop v, int num) { return v.Area - num; }
+        public static bool operator ==(Shop v, int num) { return v.Area == num; }
+        public static bool operator !=(Shop v, int num) { return v.Area != num; }
+        public static bool operator >(Shop v, int num) { return v.Area > num; }
+        public static bool operator <(Shop v, int num) { return v.Area < num; }
+    }
+}
