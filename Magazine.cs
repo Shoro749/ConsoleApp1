5a19d6dbe602048b2ecd33d91b331a23135fa34f
Shoro749 <romanbro749@gmail.com>
2023-07-28 17:35:49 +0300

    Task 1 completed

---
+++ Magazine.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*В одному з попередніх практичних завдань ви створювали клас «Журнал». Додайте до вже створеного класу
+інформацію про кількість працівників. Виконайте навантаження + (для збільшення кількості працівників на вказану кількість), — (для зменшення кількості працівників
+на вказану кількість), == (перевірка на рівність кількості
+працівників), < і > (перевірка на меншу чи більшу кількість працівників), != і Equals. Використовуйте механізм
+властивостей полів класу.*/
+
+namespace ConsoleApp1
+{
+    public class Magazine
+    {
+        private string _name;
+        private int _year;
+        private string _description;
+        private string _telephone;
+        private string _email;
+        private int _employees;
+
+        public Magazine()
+        {
+            _name = "Noname";
+            _year = 0000;
+            _description = "No_description";
+            _telephone = "+3800000000000";
+            _email = "No_email";
+            _employees = 0;
+        }
+        public Magazine(string name, int year, string description, string tel, string email, int employees)
+        {
+            _name = name;
+            _year = year;
+            _description = description;
+            _telephone = "+380" + tel;
+            _email = email;
+            _employees = employees;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _name = value;
+            }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                if (value < 1000 || value > 9999)
+                    throw new Exception("Введено неправильний рік!");
+                else
+                    _year = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _description = value;
+            }
+        }
+
+        public string Telephone
+        {
+            get { return _telephone; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _telephone = "+380" + value;
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (value is null)
+                    throw new Exception("Введено пусте поле!");
+                else
+                    _email = value;
+            }
+        }
+
+        public int Employees
+        {
+            get { return _employees; }
+            set { _employees = value; }
+        }
+
+        public static int operator +(Magazine v, int num) { return v.Employees + num; }
+        public static int operator -(Magazine v, int num) { return v.Employees - num; }
+        public static bool operator ==(Magazine v, int num) { return v.Employees == num; }
+        public static bool operator !=(Magazine v, int num) { return v.Employees != num; }
+        public static bool operator >(Magazine v, int num) { return v.Employees > num; }
+        public static bool operator <(Magazine v, int num) { return v.Employees < num; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,111 @@ namespace ConsoleApp1
         {
             try
             {
+                /*В одному з попередніх практичних завдань ви створювали клас «Журнал». Додайте до вже створеного класу
+                інформацію про кількість працівників. Виконайте навантаження + (для збільшення кількості працівників на вказану кількість), — (для зменшення кількості працівників
+                на вказану кількість), == (перевірка на рівність кількості
+                працівників), < і > (перевірка на меншу чи більшу кількість працівників), != і Equals. Використовуйте механізм
+                властивостей полів класу.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
+                Magazine v = new Magazine("GoodMorning", 2016, "Цей журнал розповідає про новини в світі!", "0986782166", "goodmorning@gmail.com", 12);
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
-
-                if (num > 0 && num < 100)
+                while (true)
                 {
-                    if (num % 3 == 0)
+                    bool Exit = false;
+                    Console.WriteLine("\n1) Змінити назву журналу");
+                    Console.WriteLine("2) Змінити рік випуску");
+                    Console.WriteLine("3) Змінити опис журналу");
+                    Console.WriteLine("4) Змінити контактний номер журналу");
+                    Console.WriteLine("5) Змінити еmail журналу");
+                    Console.WriteLine("6) Змінити кількість працівників");
+                    Console.WriteLine("7) Порівняти кількість працівників");
+                    Console.WriteLine("8) Показати інформацію про журнал");
+                    Console.WriteLine("9) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            Console.Write("\nВведіть нову назву журналу: ");
+                            v.Name = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 2:
+                            Console.Write("\nВведіть новий рік випуску: ");
+                            v.Year = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.Write("\nВведіть новий опис журналу: ");
+                            v.Description = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 4:
+                            Console.Write("\nВведіть новий контактний номер: ");
+                            v.Telephone = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 5:
+                            Console.Write("\nВведіть новий еmail: ");
+                            v.Email = Convert.ToString(Console.ReadLine());
+                            break;
+
+                        case 6:
+                            Console.Write("\nВведіть 1 щоб збільшити, або 2 щоб зменшити кількість працівників: ");
+                            select = Convert.ToInt32(Console.ReadLine());
+                            int num;
+
+                            if (select == 1)
+                            {
+                                Console.Write("\nВведіть число: ");
+                                num = Convert.ToInt32(Console.ReadLine());
+                                v.Employees = v.Employees + num;
+                            }
+                            else
+                            {
+                                Console.Write("\nВведіть число: ");
+                                num = Convert.ToInt32(Console.ReadLine());
+                                v.Employees = v.Employees - num;
+                            }
+                            break;
+
+                        case 7:
+                            Console.Write("\nВведіть число: ");
+                            select = Convert.ToInt32(Console.ReadLine());
+
+                            if (v.Employees == select)
+                            {
+                                Console.WriteLine($"\nЧисло працівників дорінює вашому числу!");
+                            }
+                            else if (v.Employees > select)
+                            {
+                                Console.WriteLine($"\nЧисло працівників більша ніж {select}!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"\nЧисло працівників менша ніж {select}!");
+                            }
+                            break;
+
+                        case 8:
+                            Console.WriteLine($"\nНазва журналу: {v.Name}\nРік випуску: {v.Year}" +
+                                $"\nОпис: {v.Description}\nКонтактний номер: {v.Telephone}\nЕmail: {v.Email}\n" +
+                                $"Кількість працівників: {v.Employees}\n");
+                            break;
+
+                        case 9:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
