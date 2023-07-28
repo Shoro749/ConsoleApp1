6358c39fa0da43dd163f99f2fcf8a04724fc3ace
Shoro749 <romanbro749@gmail.com>
2023-07-28 22:26:38 +0300

    Task 4 completed

---
+++ BookList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть додаток «Список книг до прочитання». Додаток
+має дозволяти додавати книги до списку, видаляти книги
+зі списку, перевіряти чи є книга у списку, і т.д. Використовуйте механізм властивостей, навантаження операторів,
+індексаторів.*/
+
+namespace ConsoleApp1
+{
+    internal class BookList
+    {
+        private string[] _arr = new string[50];
+
+        public BookList()
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                _arr[i] = " ";
+            }
+        }
+
+        public BookList(string[] arr)
+        {
+            _arr = arr;
+        }
+
+        public string[] Arr
+        {
+            get { return _arr; }
+            set { _arr = value; }
+        }
+
+        public void AddBook(string title)
+        {
+            for (int i = 0; i < _arr.Length; i++) 
+            {
+                if (_arr[i] == " " || _arr[i] == null)
+                {
+                    _arr[i] = title;
+                    break;
+                }
+            }
+        }
+
+        public void RemoveBook(string title)
+        {
+            bool check = false;
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (_arr[i] == title)
+                {
+                    check = true;
+                    _arr[i] = " ";
+                    break;
+                }
+            }
+            if (check == false)
+            {
+                Console.WriteLine("Книги в наявності нема!");
+            }
+        }
+
+        public bool InStock(string title)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (_arr[i] == title)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,65 @@ namespace ConsoleApp1
         {
             try
             {
+                /*Створіть додаток «Список книг до прочитання». Додаток
+                має дозволяти додавати книги до списку, видаляти книги
+                зі списку, перевіряти чи є книга у списку, і т.д. Використовуйте механізм властивостей, навантаження операторів,
+                індексаторів.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                string[] arr = new string[50];
+                arr[0] = "451 degrees Fahrenheit";
+                arr[1] = "In search of lost time";
+                arr[2] = "Process";
+                arr[3] = "Outsider";
+                arr[4] = "Black Council";
 
-                if (num > 0 && num < 100)
+                BookList v = new BookList(arr);
+
+                while(true) 
                 {
-                    if (num % 3 == 0)
+                    bool Exit = false;
+                    Console.WriteLine("\n1) Додати книгу до списку");
+                    Console.WriteLine("2) Видалити книгу з списку");
+                    Console.WriteLine("3) Перевірити книгу на наявність");
+                    Console.WriteLine("4) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
                     {
-                        Console.WriteLine("Fizz");
+                        case 1:
+                            Console.WriteLine("\nВведіть назву книги: ");
+                            string ?title = Convert.ToString(Console.ReadLine());
+                            v.AddBook(title);
+                            break;
+
+                        case 2:
+                            Console.WriteLine("\nВведіть назву книги: ");
+                            title = Convert.ToString(Console.ReadLine());
+                            v.RemoveBook(title);
+                            break;
+
+                        case 3:
+                            Console.WriteLine("\nВведіть назву книги: ");
+                            title = Convert.ToString(Console.ReadLine());
+
+                            if (v.InStock(title)) { Console.WriteLine("\nКнига в наявності!"); }
+                            else { Console.WriteLine("\nКнига не в наявності!"); }
+                            break;
+
+                        case 4:
+                            Exit = true;
+                            break;
                     }
-                    else
+
+                    if (Exit)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
