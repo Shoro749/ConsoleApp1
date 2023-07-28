86ef846c60a30e5d2b981c53335744af1ae2c014
Shoro749 <romanbro749@gmail.com>
2023-07-28 10:51:08 +0300

    Task 2 completed

---
+++ Matrix.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Matrix
+    {
+        private int[,] _arr = new int[3, 3];
+        public Matrix()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _arr[i, j] = 0;
+                }
+            }
+        }
+        public Matrix(int[,] arr)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _arr[i, j] = arr[i, j];
+                }
+            }
+        }
+        public int[,] getMatrix
+        {
+            get { return _arr; }
+            set { _arr = value; }
+        }
+
+        public void ShowMatrix()
+        {
+            Console.Write("\n");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(_arr[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void WriteMatrix()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Ввеідть число: ");
+                    _arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            ShowMatrix();
+        }
+
+        public int getM()
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (_arr[i, j] < min)
+                    {
+                        min = _arr[i, j];
+                    }
+                }
+            }
+
+            return min;
+        }
+
+        public int getM(int[,] arr)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr[i, j] > max)
+                    {
+                        max = arr[i, j];
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        public int getNum(int num1, int num2) { return _arr[num1, num2]; }
+
+        public static Matrix operator +(Matrix arr, Matrix arr1)
+        {
+            Matrix result = new Matrix();
+            int[,] matrix = new int[3,3];
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0;j < 3; j++)
+                {
+                    matrix[i,j] = arr.getNum(i, j) + arr1.getNum(i, j);
+                }
+            }
+            result.getMatrix = matrix;
+            return result;
+        }
+        public static Matrix operator -(Matrix arr, Matrix arr1)
+        {
+            Matrix result = new Matrix();
+            int[,] matrix = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = arr.getNum(i, j) - arr1.getNum(i, j);
+                }
+            }
+            result.getMatrix = matrix;
+            return result;
+        }
+        public static Matrix operator *(Matrix arr, Matrix arr1)
+        {
+            Matrix result = new Matrix();
+            int[,] matrix = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = arr.getNum(i, j) * arr1.getNum(i, j);
+                }
+            }
+            result.getMatrix = matrix;
+            return result;
+        }
+        public static bool operator ==(Matrix arr, Matrix arr1)
+        {
+            bool checking = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr.getNum(i, j) != arr1.getNum(i, j))
+                    {
+                        checking = false; break;
+                    }
+                }
+            }
+
+            return checking;
+        }
+
+        public static bool operator !=(Matrix arr, Matrix arr1)
+        {
+            bool checking = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr.getNum(i, j) != arr1.getNum(i, j))
+                    {
+                        checking = false; break;
+                    }
+                }
+            }
+
+            return checking;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,35 +8,113 @@ namespace ConsoleApp1
         {
             try
             {
+                /*В одному з попередніх практичних завдань ви створювали клас «Матриця». Виконайте перевантаження +
+                (для додавання матриць), – (для віднімання матриць). *
+                (множення матриць одна на одну, множення матриці на
+                число), == (перевірка матриць на рівність), != і Equals.
+                Використовуйте механізм властивостей полів класу і механізм індексаторів.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
+                Random rnd = new Random();
 
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                int[,] arr = new int[3, 3];
+                int[,] arr1 = new int[3, 3];
 
-                if (num > 0 && num < 100)
+                for (int i = 0; i < 3; i++)
                 {
-                    if (num % 3 == 0)
+                    for (int j = 0; j < 3; j++)
                     {
-                        Console.WriteLine("Fizz");
+                        arr[i, j] = rnd.Next(0, 100);
+                        Console.Write(arr[i, j] + " ");
                     }
-                    else
+                    Console.Write("\n");
+                }
+
+                Console.Write("\n");
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
                     {
-                        if (num % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(num);
-                        }
+                        arr1[i, j] = rnd.Next(0, 100);
+                        Console.Write(arr1[i, j] + " ");
+                    }
+                    Console.Write("\n");
+                }
+
+                var v = new Matrix(arr);
+                var q = new Matrix(arr1);
+
+                while (true)
+                {
+                    bool Exit = false;
+                    Console.WriteLine("\n1) Показати матрицю");
+                    Console.WriteLine("2) Переписати матрицю");
+                    Console.WriteLine("3) Найти мінімум");
+                    Console.WriteLine("4) Найти максимум");
+                    Console.WriteLine("5) Додати матриці");
+                    Console.WriteLine("6) Відняти матриці");
+                    Console.WriteLine("7) Множення матриці");
+                    Console.WriteLine("8) Порівняти матриці");
+                    Console.WriteLine("9) Вихід");
+                    Console.Write("Введіть свій вибір: ");
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
+                    {
+                        case 1:
+                            v.ShowMatrix();
+                            break;
+
+                        case 2:
+                            v.WriteMatrix();
+                            break;
+
+                        case 3:
+                            Console.WriteLine("\nМінімум: " + v.getM());
+                            break;
+
+                        case 4:
+                            Console.WriteLine("\nМаксимум: " + v.getM(arr));
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Результат: ");
+                            Matrix new_matrix = new Matrix();
+                            new_matrix = v + q;
+                            new_matrix.ShowMatrix();
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Результат: ");
+                            Matrix n_matrix = new Matrix();
+                            n_matrix = v - q;
+                            n_matrix.ShowMatrix();
+                            break;
+
+                        case 7:
+                            Console.WriteLine("Результат: ");
+                            Matrix nw_matrix = new Matrix();
+                            nw_matrix = v * q;
+                            nw_matrix.ShowMatrix();
+                            break;
+
+                        case 8:
+                            if (v == q) { Console.WriteLine("\nМатриці однакові!"); }
+                            else { Console.WriteLine("\nМатриці не однакові!"); }
+                            break;
+
+                        case 9:
+                            Exit = true;
+                            break;
+                    }
+
+                    if (Exit)
+                    {
+                        break;
                     }
                 }
-                else
-                {
-                    throw new Exception("Число не в діапазоні від 1 до 100!");
-                }
-                
             }
             catch (Exception ex)
             {
